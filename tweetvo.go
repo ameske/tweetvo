284f34051e9a8f30c7c13493fad4441a18901874
Kyle Ames <kyle@ameske.org>
2014-12-20 10:00:40 -0500

    Implemented basic structure of streaming pipleline

--- tweetvo.go
+++ tweetvo.go
@@ -1,25 +1,82 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
+	"fmt"
 	"io/ioutil"
 	"log"
+	"net/http"
 
 	"gopkg.in/yaml.v2"
 )
 
-func main() {
-	// Read in OAuth information from a file to prevent leaking it on github ;-)
+var (
+	config OAuthConfig
+)
+
+func init() {
 	c, err := ioutil.ReadFile("tweetvo.yaml")
 	if err != nil {
 		log.Fatalf(err.Error())
 	}
 
-	config := &OAuthConfig{}
-	err = yaml.Unmarshal(c, config)
+	err = yaml.Unmarshal(c, &config)
 	if err != nil {
 		log.Fatalf(err.Error())
 	}
+}
 
-	api := NewTwitterClient(config)
-	log.Printf("%v", api)
+func main() {
+	streaming()
+}
+
+func streaming() {
+	req, err := http.NewRequest("GET", "https://userstream.twitter.com/1.1/user.json?with=true", nil)
+	if err != nil {
+		log.Fatalf("NewRequest: %s", err.Error())
+	}
+
+	err = config.Sign(req, config.header())
+	if err != nil {
+		log.Fatalf("Sign: %s", err.Error())
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Fatalf("Do: %s", err.Error())
+	}
+	defer resp.Body.Close()
+
+	respReader := bufio.NewReader(resp.Body)
+	for {
+		message, err := readMessage(respReader)
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+
+		go processMessage(message)
+	}
+}
+
+func readMessage(r *bufio.Reader) ([]byte, error) {
+	var msg bytes.Buffer
+
+	isPrefix := true
+	for isPrefix {
+		var err error
+		var bytes []byte
+		bytes, isPrefix, err = r.ReadLine()
+		if err != nil {
+			return nil, err
+		}
+
+		msg.Write(bytes)
+	}
+
+	return msg.Bytes(), nil
+}
+
+func processMessage(message []byte) {
+	fmt.Printf("%s\n", string(message))
 }
